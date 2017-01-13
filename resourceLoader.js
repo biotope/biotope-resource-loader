4f508946a99003878f13d3b96c04f8c68511fba4
Saeid Mohadjer <saeid@fastmail.fm>
2017-01-13 09:55:37 +0100

    fixed jshint issues

--- resourceLoader.js
+++ resourceLoader.js
@@ -67,9 +67,11 @@ function resourceLoader(options) {
 		// create unique absolute path
 		normalizedPath = absolutePath(path);
 
-		(_debug) ? console.log('inputPath: ' + path) : '';
-		(_debug) ? console.log('normalizedPath: ' + normalizedPath) : '';
-		(_debug) ? console.log('------') : '';
+		if (_debug) {
+			console.log('inputPath: ' + path);
+			console.log('normalizedPath: ' + normalizedPath);
+			console.log('------');
+		}
 
 		return normalizedPath;
 	};
@@ -95,23 +97,16 @@ function resourceLoader(options) {
 	var addPathToQueue = function(path, resource) {
 		var queueObject = {};
 
-		path = normalizePath(path, resource);
-
-		// determine file type
-		if (path.indexOf('.css') > -1) {
-			queueObject['type'] = 'css';
-		} else {
-			queueObject['type'] = 'js';
-		}
-		queueObject['uniquePath'] = path;
+		queueObject.type = path.indexOf('.css') > -1 ? 'css' : 'js';
+		queueObject.uniquePath = normalizePath(path, resource);
 
 		// check depending on files
 		if (resource.dependsOn) {
-			queueObject['dependingOnFiles'] = [];
+			queueObject.dependingOnFiles = [];
 
 			$.each(resource.dependsOn, function () {
 				var dependsOnPath = normalizePath(this, resource);
-				queueObject['dependingOnFiles'].push(dependsOnPath);
+				queueObject.dependingOnFiles.push(dependsOnPath);
 			});
 		}
 
@@ -128,23 +123,23 @@ function resourceLoader(options) {
 
 		for (var iterator = 0; iterator < tempLength; iterator++) {
 			_tempQueue.forEach(function (qO, index) {
-				if (!qO['dependingOnFiles'] || qO['dependingOnFiles'].length === 0) {
-					_loadedQueue.push(qO['uniquePath']);
+				if (!qO.dependingOnFiles || qO.dependingOnFiles.length === 0) {
+					_loadedQueue.push(qO.uniquePath);
 					_tempQueue.splice(index, 1);
 				}
 			});
 
 			_tempQueue.forEach(function (qO, index) {
-				if (_loadedQueue.indexOf(qO['uniquePath']) !== -1) {
+				if (_loadedQueue.indexOf(qO.uniquePath) !== -1) {
 					_tempQueue.splice(index, 1);
 				}
 			});
 
 			_tempQueue.forEach(function (qO, index) {
-				if (qO['dependingOnFiles']) {
-					qO['dependingOnFiles'].forEach(function (dependency, innerIndex) {
+				if (qO.dependingOnFiles) {
+					qO.dependingOnFiles.forEach(function (dependency, innerIndex) {
 						if (_loadedQueue.indexOf(dependency) !== -1) {
-							qO['dependingOnFiles'].splice(innerIndex, 1)
+							qO.dependingOnFiles.splice(innerIndex, 1);
 						}
 					});
 				}
@@ -154,7 +149,7 @@ function resourceLoader(options) {
 		// check if any unresolvable dependencies are set
 		if (_tempQueue.length > 0) {
 			_tempQueue.forEach(function (qO) {
-				console.warn('Error: Unresolved dependency for ' + qO['uniquePath'] + ' detected. Dependencies: ' + qO['dependingOnFiles']);
+				console.warn('Error: Unresolved dependency for ' + qO.uniquePath + ' detected. Dependencies: ' + qO.dependingOnFiles);
 			});
 		}
 	};
@@ -165,7 +160,10 @@ function resourceLoader(options) {
 	var loadResources = function () {
 		// if queue is empty, trigger ready
 		var checkIfComplete = function () {
-			(_debug) ? console.log('Remaining queue: ', _queue) : '';
+			if (_debug) {
+				console.log('Remaining queue: ', _queue);
+			}
+
 			if (_queue.length === 0) {
 				$(window).trigger('resourcesReady');
 			}
@@ -178,25 +176,29 @@ function resourceLoader(options) {
 
 		// create promise for each unique queue object (path as unique id)
 		_queue.forEach(function (outerQueueObject, index) {
-			outerQueueObject['promise'] = $.Deferred();
-			(_debug) ? outerQueueObject['promiseName'] = 'promise' + index : '';
+			outerQueueObject.promise = $.Deferred();
+			if (_debug) {
+				outerQueueObject.promiseName = 'promise' + index;
+			}
 
 			_queue.forEach(function (innerQueueObject) {
-				if (innerQueueObject['uniquePath'] === outerQueueObject['uniquePath']) {
-					innerQueueObject['promise'] = outerQueueObject['promise'];
-					(_debug) ? innerQueueObject['promiseName'] = outerQueueObject['promiseName'] : '';
+				if (innerQueueObject.uniquePath === outerQueueObject.uniquePath) {
+					innerQueueObject.promise = outerQueueObject.promise;
+					if (_debug) {
+						innerQueueObject.promiseName = outerQueueObject.promiseName;
+					}
 				}
-			})
+			});
 		});
 
 		// add dependencies
 		_queue.forEach(function (queueObject) {
-			queueObject['dependencies'] = [];
-			if (queueObject['dependingOnFiles']) {
-				queueObject['dependingOnFiles'].forEach(function (dependency) {
+			queueObject.dependencies = [];
+			if (queueObject.dependingOnFiles) {
+				queueObject.dependingOnFiles.forEach(function (dependency) {
 					_queue.forEach(function (queueObjectInner) {
-						if (queueObjectInner['uniquePath'] === dependency) {
-							(queueObject['dependencies'].push(queueObjectInner['promise']));
+						if (queueObjectInner.uniquePath === dependency) {
+							(queueObject.dependencies.push(queueObjectInner.promise));
 							return;
 						}
 					});
@@ -207,53 +209,65 @@ function resourceLoader(options) {
 		// execute dependencies
 		_queue.forEach(function (queueObject) {
 
-			$.when.apply($, queueObject['dependencies']).done(function () {
+			$.when.apply($, queueObject.dependencies).done(function () {
 				var cachePromise;
 
-				if (_cache[queueObject['uniquePath']]) {
-					queueObject['promise'].resolve();
-					(_debug) ? console.log('Promise for ' + queueObject['uniquePath'] + ' resolved from cache.') : '';
+				if (_cache[queueObject.uniquePath]) {
+					queueObject.promise.resolve();
+
+					if (_debug) {
+						console.log('Promise for ' + queueObject.uniquePath + ' resolved from cache.');
+					}
 				} else {
 					// load js file
-					if (queueObject['type'] === 'js') {
+					if (queueObject.type === 'js') {
 						cachePromise = $.ajax({
-							url: queueObject['uniquePath'],
+							url: queueObject.uniquePath,
 							dataType: 'script',
 							cache: true
 						}).always(function () {
-							queueObject['promise'].resolve();
+							queueObject.promise.resolve();
 							_queue = _queue.filter(function (item) {
-								return item['uniquePath'] !== queueObject['uniquePath'];
+								return item.uniquePath !== queueObject.uniquePath;
 							});
 
-							(_debug) ? console.log('Promise for ' + queueObject['uniquePath'] + ' resolved.') : '';
+							if (_debug) {
+								console.log('Promise for ' + queueObject.uniquePath + ' resolved.');
+							}
+
 							checkIfComplete();
 						}).fail(function () {
-							console.warn('Error while loading: ' + queueObject['uniquePath']);
+							console.warn('Error while loading: ' + queueObject.uniquePath);
 						});
 					}
 					// load css file
-					if (queueObject['type'] === 'css') {
+					if (queueObject.type === 'css') {
 						var $link = $('<link/>', {
 							rel: 'stylesheet',
 							type: 'text/css',
-							href: queueObject['uniquePath']
+							href: queueObject.uniquePath
 						}).on('load', function () {
-							queueObject['promise'].resolve();
+							queueObject.promise.resolve();
 							_queue = _queue.filter(function (item) {
-								return item['uniquePath'] !== queueObject['uniquePath'];
+								return item.uniquePath !== queueObject.uniquePath;
 							});
 
-							(_debug) ? console.log('Promise for ' + queueObject['uniquePath'] + ' resolved.') : '';
+							if (_debug) {
+								console.log('Promise for ' + queueObject.uniquePath + ' resolved.');
+							}
+
 							checkIfComplete();
 						}).on('error', function () {
-							console.warn('Error while loading: ' + queueObject['uniquePath']);
-							queueObject['promise'].resolve();
+							console.warn('Error while loading: ' + queueObject.uniquePath);
+							queueObject.promise.resolve();
 							_queue = _queue.filter(function (item) {
-								return item['uniquePath'] !== queueObject['uniquePath'];
+								return item.uniquePath !== queueObject.uniquePath;
 							});
 
-							(_debug) ? console.log('Promise for ' + queueObject['uniquePath'] + ' resolved.') : '';
+							if (_debug) {
+								console.log('Promise for ' + queueObject.uniquePath + ' resolved.');
+							}
+
 							checkIfComplete();
 						});
 
@@ -261,7 +275,7 @@ function resourceLoader(options) {
 						cachePromise = $.Deferred().resolve();
 					}
 
-					_cache[queueObject['uniquePath']] = cachePromise;
+					_cache[queueObject.uniquePath] = cachePromise;
 				}
 			});
 		});
