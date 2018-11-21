9d296926e2062527425524284a30171f0ba0191a
SheepFromHeaven <marcwissler@gmail.com>
2018-11-21 15:56:34 +0100

    Add tests for getResourceElements

--- src/getResourceElements.spec.ts
+++ src/getResourceElements.spec.ts
@@ -1,9 +1,25 @@
 import { expect } from 'chai';
 import getResourceElements from './getResourceElements';
 
-describe.skip('#getResourceElements', () => {
+describe('#getResourceElements', () => {
 
     it('returns empty array for non existent resource elements', () => {
-        expect(false).to.be.true
+        const resourceElements = getResourceElements(undefined);
+        expect(resourceElements).to.be.empty;
+    });
+
+    it('returns an empty array for conteiner with no resource elements', () => {
+        const container = document.createElement('div');
+        const resourceElements = getResourceElements(container);
+        expect(resourceElements).to.be.empty;
+    });
+
+    it('returns an array with all resource elements', () => {
+        const container = document.createElement('div');
+        const resourceTemplate = new Array(5).fill('<div data-resources="{paths: [{}]}"></div>').join('');
+        container.innerHTML = resourceTemplate;
+
+        const resourceElements = getResourceElements(container);
+        expect(resourceElements).to.have.lengthOf(5)
     });
 })
--- test/help.js
+++ test/help.js
@@ -1,4 +1,10 @@
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 chai.use(sinonChai);
+
+const dom = new JSDOM();
+
+global.document = dom.window.document;
