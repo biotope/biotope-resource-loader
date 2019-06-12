f60c1d521e3d66c0287602bfe4d37bd03b8fc3d7
Tiago Aguiar <51372512+tiagoaguiar31@users.noreply.github.com>
2019-06-12 08:20:25 +0100

    Fix issue #35 - resourceReady not firing (#36)

--- src/fp/remove.ts
+++ src/fp/remove.ts
@@ -1,6 +1,6 @@
 import curry from './curry';
 
-const remove = (start: number, deleteCount: number, arr: ReadonlyArray<any>) => {
+const remove = (start: number, deleteCount: number, arr: Array<any>) => {
 	const newArr = [...arr];
 	newArr.splice(start, deleteCount);
 	return newArr;
