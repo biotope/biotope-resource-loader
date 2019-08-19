d8342b844a96f2f040be071c5606681d2dda65a6
Kai Wißler <kai.wissler@schwarzfisch.virtual-identity.net>
2019-08-19 08:19:28 +0200

    fix: Fix html file path regex

--- src/pathManagement/isHtmlFilePath.ts
+++ src/pathManagement/isHtmlFilePath.ts
@@ -1,3 +1,6 @@
-const isHtmlFilePath = (path: string): boolean => path.indexOf('.html') === path.length - 5;
+const isHtmlFilePath = (path: string): boolean =>
+	/^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?([a-zA-Z0-9\-\.\?\!\,\'\/\\\+&amp;%\$#_]*)?$/gm.test(
+		path
+	);
 
 export default isHtmlFilePath;
