900f682c8ca35b506fd547d7dfed8fad3664f91c
César Marques <cesar.marques@virtual-identity.com>
2019-10-14 11:42:33 +0100

    Update getComponentFromQueue

--- src/dom/getComponentQueueFromElement.ts
+++ src/dom/getComponentQueueFromElement.ts
@@ -1,8 +1,9 @@
 import { HTMLComponentDefinition } from './../types';
 import getDataResourceFromElement from './getDataResourceFromElement';
 import { ComponentDefinition } from '../types';
+import { defaultOptions } from '../defaultOptions';
 
-const getComponentQueueFromElement = (element: HTMLElement = document.createElement('div'), resourceListAtrributeSelector: string): HTMLComponentDefinition[] => {
+const getComponentQueueFromElement = (element: HTMLElement = document.createElement('div'), resourceListAtrributeSelector: string = defaultOptions.resourceListAtrributeSelector): HTMLComponentDefinition[] => {
     const dataResources: string = getDataResourceFromElement(element, resourceListAtrributeSelector);
     const definitions: ComponentDefinition[] = eval(dataResources) || [];
 
