0ca26d04d27b9744136dda6a4a3f79c23cb0ba61
César Marques <cesar.marques@virtual-identity.com>
2020-05-28 21:53:27 +0100

    Add scriptParsed event emission

--- src/handlers/jsHandler.ts
+++ src/handlers/jsHandler.ts
@@ -3,10 +3,18 @@ import { Handler } from '../types';
 
 const isJs = (options: HandleOptions): boolean => options.resource.path.indexOf('.js') > -1;
 
+const emitScriptParsed = (nodes: HTMLElement[]) => {
+  const scriptParsedEvent: CustomEvent = new CustomEvent('scriptParsed', { bubbles: true });
+  nodes.forEach((node: Node) => {
+    node.dispatchEvent(scriptParsedEvent);
+  });
+}
+
 const onJsLoaded = (options: HandleOptions): void => {
     const script: HTMLScriptElement = document.createElement('script');
     script.src = options.resource.path;
     script.async = true;
+    script.onload = () => emitScriptParsed(options.resource.elements);
     document.body.append(script);
 }
 
