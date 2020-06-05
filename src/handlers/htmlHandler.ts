39ed0e4d7b118b9ac9124dbe39da4c83d0ebf7ad
César Marques <cesar.marques@virtual-identity.com>
2020-06-05 16:04:37 +0100

    Update handlers to fire contextual ready events

--- src/handlers/htmlHandler.ts
+++ src/handlers/htmlHandler.ts
@@ -4,6 +4,11 @@ import { Handler } from '../types';
 const isHtml = (options: HandleOptions): boolean => options.resource.path
   .slice(-'.html'.length) === '.html';
 
+const emitHtmlReady = (node: HTMLElement, htmlReadyEventName) => {
+  const scriptParsedEvent: CustomEvent = new CustomEvent(htmlReadyEventName, { bubbles: true });
+  node.dispatchEvent(scriptParsedEvent);
+}
+
 const onHtmlLoaded = (options: HandleOptions): void => {
   const elements: HTMLElement[] = [].slice.call(options.resource.elements);
 
@@ -11,6 +16,7 @@ const onHtmlLoaded = (options: HandleOptions): void => {
       const fragment = document.createRange().createContextualFragment(stringOfHtml);
       elements.forEach((element: HTMLElement) => {
         element.appendChild(fragment);
+        emitHtmlReady(element, options.htmlReadyEventName);
       });
     });
 }
--- src/handlers/jsHandler.ts
+++ src/handlers/jsHandler.ts
@@ -3,8 +3,8 @@ import { Handler } from '../types';
 
 const isJs = (options: HandleOptions): boolean => options.resource.path.indexOf('.js') > -1;
 
-const emitScriptParsed = (nodes: HTMLElement[], scriptsParsedEvent) => {
-  const scriptParsedEvent: CustomEvent = new CustomEvent(scriptsParsedEvent, { bubbles: true });
+const emitScriptReady = (nodes: HTMLElement[], scriptReadyEventName) => {
+  const scriptParsedEvent: CustomEvent = new CustomEvent(scriptReadyEventName, { bubbles: true });
   nodes.forEach((node: Node) => {
     node.dispatchEvent(scriptParsedEvent);
   });
@@ -14,7 +14,7 @@ const onJsLoaded = (options: HandleOptions): void => {
   const script: HTMLScriptElement = document.createElement('script');
   script.src = options.resource.path;
   script.async = true;
-  script.onload = () => emitScriptParsed(options.resource.elements, options.scriptParsedEvent);
+  script.onload = () => emitScriptReady(options.resource.elements, options.scriptReadyEventName);
   document.body.append(script);
 }
 
