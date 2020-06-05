05b6c07457574e7aa723acbc326aa475e9237824
César Marques <cesar.marques@virtual-identity.com>
2020-06-05 10:36:25 +0100

    Fix source of module and legacy exports

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,6 +1,6 @@
-import { HandleOptions, Handler } from '../types';
-import './Object.assign';
-import './fetch';
+// import { HandleOptions, Handler } from '../types';
+// import './Object.assign';
+// import './fetch';
 import './Array.find';
 import './Promise';
 import './CustomEvent';
@@ -8,46 +8,8 @@ import { ResourceLoader } from './../ResourceLoader';
 import { ResourceLoaderOptions } from '../types';
 
 window['resourceLoader'] = function (options: ResourceLoaderOptions) {
-
-    const cssHandler: Handler = {
-        match: function (options: HandleOptions) { return options.resource.path.indexOf('.css') > -1 },
-        handle: function (options: HandleOptions) {
-            const style = document.createElement('link');
-            style.rel = 'stylesheet';
-            style.href = options.resource.path;
-            document.body.appendChild(style);
-        }
-    }
-
-    const jsHandler: Handler = {
-        match: function (options: HandleOptions) { return options.resource.path.indexOf('.js') > -1 },
-        handle: function (options: HandleOptions) {
-            const script = document.createElement('script');
-            script.src = options.resource.path;
-            script.async = true;
-            document.body.appendChild(script);
-        }
-    }
-
-    const htmlHandler: Handler = {
-        match: function (options: HandleOptions) { return options.resource.path.indexOf('.html') > -1 },
-        handle: function (options: HandleOptions) {
-            options.response.text().then((text: string) => {
-                options.resource.elements.forEach((element: HTMLElement) => {
-                    element.innerHTML = text;
-                    loader.update();
-                });
-            });
-        }
-    }
-
     const loader = new ResourceLoader({
-        handler: [
-            cssHandler,
-            jsHandler,
-            htmlHandler
-        ],
-        ...options,
+      ...options,
     });
 
     return loader;
