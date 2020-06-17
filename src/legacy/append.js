026c2288e68b868295d5bf5cbbc279ae1fc8eae0
César Marques <cesar.marques@virtual-identity.com>
2020-06-17 14:16:58 +0100

    Add ie11 polyfills (#188)

---
+++ src/legacy/append.js
@@ -0,0 +1,25 @@
+// Taken from https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/append#Polyfill
+// Source: https://github.com/jserz/js_piece/blob/master/DOM/ParentNode/append()/append().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('append')) {
+      return;
+    }
+    Object.defineProperty(item, 'append', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function append() {
+        var argArr = Array.prototype.slice.call(arguments),
+          docFrag = document.createDocumentFragment();
+
+        argArr.forEach(function (argItem) {
+          var isNode = argItem instanceof Node;
+          docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+        });
+
+        this.appendChild(docFrag);
+      }
+    });
+  });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
